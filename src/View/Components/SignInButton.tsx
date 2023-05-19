6d43da5b4cfdc6e927975402bf58f8f27a693514
Vincent Vermeire <vvermeire55@gmail.com>
2023-05-19 19:22:17 +0200

    auth and access token persistence

--- src/View/Components/SignInButton.tsx
+++ src/View/Components/SignInButton.tsx
@@ -1,25 +1,37 @@
 import { getApp } from "firebase/app";
-import { OAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { OAuthProvider, browserLocalPersistence, getAuth, signInWithPopup } from "firebase/auth";
+import { redirect } from "react-router-dom";
 
 export default function SignInButton() {
+  
+  const saveAccessTokenInLocalStorage = (accessToken: string | undefined) => {
+    if (!accessToken) {
+      return;
+    }
+    localStorage.setItem('accessToken', accessToken);
+  }
 
-  function signIn() {
+
+  const signIn = () => {
     const provider = new OAuthProvider('microsoft.com');
     provider.setCustomParameters({
       prompt: 'select_account',
       tenant: '32df73d7-fe92-47d0-9749-9d50d58e4f0c'
     });
-
     const app = getApp();
     const auth = getAuth(app);
 
+    auth.setPersistence(browserLocalPersistence);
+
     signInWithPopup(auth, provider)
-      .then(() => {
-        window.location.href = '/';
+      .then((result) => {
+        saveAccessTokenInLocalStorage(OAuthProvider.credentialFromResult(result)?.accessToken)
+        return redirect('/')
+
       }).catch((error) => {
         alert("Something went wrong. Please try again. (Error: " + error.code + ")");
     });
-  }
+  };
 
   return (
     <button
